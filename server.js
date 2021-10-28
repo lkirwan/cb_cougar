574d9995daade5897859905d01768568b8a82622
lkirwan <liam.p.kirwan@gmail.com>
2021-10-27 23:25:14 -0400

    improve logging

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const app = require("./app");
 var port = process.env.PORT || 3006;
 
-app.listen(port, () => console.log(`server starting on port ${port}`));
+app.listen(port, () => console.info(`server starting on port ${port}`));
